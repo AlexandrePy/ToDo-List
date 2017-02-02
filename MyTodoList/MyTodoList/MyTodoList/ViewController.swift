dbc8a650e48f3ae3fea1d05a7eb71898d3215a8f
Robinson Prada Mejia <rdprada@MacBook-de-Robinson.local>
2017-02-02 20:24:06 +0100

    Alex: finished creating the folder

---
+++ MyTodoList/MyTodoList/MyTodoList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTodoList
+//
+//  Created by Robinson Prada Mejia on 02/02/17.
+//  Copyright © 2017 AlexandrePy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
