5b55e366ba881a445f6f422e0f4468d8ce77cefd
Robinson Prada Mejia <rdprada@MacBook-de-Robinson.local>
2017-02-04 16:08:15 +0100

    Alex: first version ok

--- CheckItOff/CheckItOff/ViewController.swift
+++ CheckItOff/CheckItOff/ViewController.swift
@@ -7,14 +7,85 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var tasks: [NSManagedObject] = []
+    
+    @IBAction func addName(_ sender: Any) {
+        
+        let alertCt = UIAlertController(title: "New Task", message: "Add a new task", preferredStyle: .alert)
+        
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) {
+            [unowned self] action in
+            guard let textField = alertCt.textFields?.first,
+                let taskToSave = textField.text else {
+                    return
+                }
+            self.saveTask(name: taskToSave)
+            self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default)
+        
+        alertCt.addTextField()
+        
+        alertCt.addAction(saveAction)
+        alertCt.addAction(cancelAction)
+        
+        present(alertCt, animated: true)
+        
+    }
+    
+    func saveTask(name: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Task", in: managedContext)!
+        
+        let task = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        task.setValue(name, forKeyPath: "name")
+        
+        do {
+            try managedContext.save()
+            tasks.append(task)
+        } catch let error as NSError {
+            print("Could not save \(error), \(error.userInfo)")
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        title = "Groceries"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Task")
+        
+        do {
+            tasks = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -23,3 +94,18 @@ class ViewController: UIViewController {
 
 }
 
+// MARK: UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let task = tasks[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = task.value(forKeyPath: "name") as? String
+        return cell
+    }
+    
+}
